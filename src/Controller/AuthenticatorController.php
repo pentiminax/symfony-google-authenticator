cb6249d7de3a39d5228914a99b386822b0e34307
pentiminax <tanguy.lemarie.youtube@outlook.fr>
2023-09-14 16:08:33 +0200

    Prepare for intro branch

--- src/Controller/AuthenticatorController.php
+++ src/Controller/AuthenticatorController.php
@@ -3,10 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Service\AuthenticatorService;
-use OTPHP\TOTP;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,48 +12,16 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class AuthenticatorController extends AbstractController
 {
-    public function __construct(
-        private readonly AuthenticatorService $authenticatorService
-    )
-    {
-    }
 
     #[Route('/authenticator/pair', name: 'app_authenticator_pair')]
-    public function pair(Request $request): Response
+    public function pair(): Response
     {
-        if ($request->isMethod(Request::METHOD_POST)) {
-            $secret = $request->request->get('secret');
-
-            $this->authenticatorService->validatePairing($this->getUser(), $secret);
-
-            return $this->redirectToRoute('app_home_index');
-        }
-
-        [$qrCodeUri, $secret] = $this->authenticatorService->getQrCodeUri($this->getUser());
-
-        return $this->render('authenticator/pair.html.twig', [
-            'qrCodeUri' => $qrCodeUri,
-            'secret' => $secret
-        ]);
+        return $this->render('authenticator/pair.html.twig');
     }
 
     #[Route('/authenticator/verify', name: 'app_authenticator_verify')]
-    public function verify(Request $request): Response
+    public function verify(): Response
     {
-        if (null === $this->getUser()->getSecret()) {
-            return $this->redirectToRoute('app_authenticator_pair');
-        }
-
-        if ($request->isMethod(Request::METHOD_POST)) {
-            $totp = TOTP::createFromSecret($this->getUser()->getSecret());
-
-            $otp = $request->request->get('otp');
-
-            $result = $totp->verify($otp);
-        }
-
-        return $this->render('authenticator/verify.html.twig', [
-            'result' => $result ?? null
-        ]);
+        return $this->render('authenticator/verify.html.twig');
     }
 }
